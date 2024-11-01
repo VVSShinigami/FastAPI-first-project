b0d65536d5e8994d7c9e11beb14555073f145a55
VVSShinigami <Kolan5814@gmail.com>
2024-11-01 20:24:00 +0300

    new com

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -4,3 +4,9 @@ from database import Base
 
 class Todos(Base):
     __tablename__ = "todos"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
